d482834d7de7e32f07c36e53abd921f16301eea0
sanjayT-2 <sanjaythirumal0209@gmail.com>
2024-08-30 09:02:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (41.27%)

---
+++ problems/0507-perfect-number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int num)
+     {
+        if(num==6 || num==28 || num==496 || num==8128 || num== 33550336)
+        {
+          
+             return true;
+        }
+        return false;
+    }
+}
