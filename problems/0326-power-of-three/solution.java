303917b834f912914ab3604018e4036f540f61bb
sanjayT-2 <sanjaythirumal0209@gmail.com>
2024-09-11 04:41:57 +0000

    [LeetCode Sync] Runtime - 8 ms (84.32%), Memory - 43.6 MB (98.92%)

---
+++ problems/0326-power-of-three/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+
+        if(n==1)
+        {
+            return true;
+        }
+        
+        while(n>0)
+        {
+            if(n%3!=0)
+            {
+                return false;
+            }
+            n/=3;
+            if(n==1)
+            {
+                return true;
+            }
+            
+        }
+        
+        return false;
+        }
+       
+        
+    }
+
