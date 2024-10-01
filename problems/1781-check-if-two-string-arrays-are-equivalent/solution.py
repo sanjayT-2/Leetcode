0a74f1c335aa2d7aa060c77a91bf15f17454f5d5
sanjayT-2 <sanjaythirumal0209@gmail.com>
2024-10-01 06:12:22 +0000

    [LeetCode Sync] Runtime - 17 ms (3.34%), Memory - 11.6 MB (100.00%)

---
+++ problems/1781-check-if-two-string-arrays-are-equivalent/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        
+        a="".join(word1)
+        b="".join(word2)
+        if(a==b):
+            return True
+        else:
+            return False
+        
