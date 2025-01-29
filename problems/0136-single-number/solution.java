4c4ef1f5546ec906d3a81d24a5c206ed80d0f20b
sanjayT-2 <sanjaythirumal0209@gmail.com>
2025-01-29 06:42:23 +0000

    [LeetCode Sync] Runtime - 1 ms (99.87%), Memory - 46.1 MB (71.94%)

--- problems/0136-single-number/solution.java
+++ problems/0136-single-number/solution.java
@@ -1,23 +1,10 @@
 class Solution {
-    public int singleNumber(int[] nums)
-     {
-        int i,j,count;
-        for(i=0;i<nums.length;i++)
-         {
-            for(j=0;j<nums.length;j++)
-             {
-                if(i!=j && nums[i]==nums[j])
-                 {
-                    break;
-                 }
-             }
-             if(j==nums.length)
-             {
-                return nums[i];
-             }
-         }
-         return 1;
-
-
+    public int singleNumber(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum^=nums[i];
+        }
+        return sum;
     }
 }
